d7064831b4f6473e65ca956ed291ff5bc3e53eab
Shaan Alam <shaanalam369@gmail.com>
2023-12-08 00:05:12 +0530

    fixed no api issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import { Button } from "@/components/ui/button";
 import { api } from "@/utils/api";
 
 export default function Home() {
-  const hello = api.post.hello.useQuery({ text: "from tRPC" });
 
   return (
     <>
